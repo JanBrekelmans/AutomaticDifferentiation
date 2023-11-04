e57ec4aa61113f240f42b2187e6ec0fa98ff301c
Jan Brekelmans <j.j.w.c.brekelmans@gmail.com>
2023-11-04 11:47:41 +0100

    Clean application folder

--- application/main.cpp
+++ application/main.cpp
@@ -1,20 +1,4 @@
 #include <AutomaticDifferentiation/Forward.h>
 
-#include <iostream>
-#include <memory>
-#include <vector>
-
-using namespace AD::Forward;
-
 int main() {
-    Dual<double> x = 3.0;
-    Dual<double> b;
-    seed(x);
-    x = x*x*x; 
-
-    auto f = IsArithmetic<AdditionExpression<double, double>&> || IsExpression<AdditionExpression<double, double>&>;
-
-
-    auto i = 1;
-    std::cout << b.a;
 }
